68f4a15c705faf0be1dd0cf7dd3f7c3cb9f1aa89
Edaly09 <edalyminthe@gmail.com>
2022-07-07 14:31:19 +0200

    printTask

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // const et variables utiles
 
 const taskArray = []
+let ulAdd = document.getElementById("ul")
+let taskCreate
+let taskWrite
 // création todo
 
 const onTaskSubmit = () => { 
-    const taskWrite = document.getElementById("taskwrite").value
-    const taskCreate = {
+     taskWrite = document.getElementById("taskwrite").value
+    taskCreate = {
         value: taskWrite,
         status: "to do"
     }
     taskArray.push(taskCreate)
     console.log(taskArray);
+    printTask()
+}
+// Affiche to do
+const printTask = () =>{
+ulAdd.innerHTML = `<li>${taskWrite} <!-- ${taskCreate.status} --> <img src="./iconesordi/Vector-2.png" alt="image de modif"> <img src="./iconesordi/Vector-1.png" alt="image de modif">  </li>`
+
 }
