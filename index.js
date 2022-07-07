ba3d80633a80378527c1d59719bc73e14260cfcb
Thomas <malifarge.thomas9@gmail.com>
2022-07-07 17:32:07 +0200

    editTask

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ let taskWrite
 const formEdit = document.getElementById("formEdit")
 let classOfTask
 let i = 0
+let taskEditValue
+let taskEditStatues
+let Limodiff
+let Doing
+let Done
 
 // création todo
 
@@ -23,21 +28,21 @@ const onTaskSubmit = () => {
 // Affiche to do
 const printTask = () =>{
 ulAdd.innerHTML += `
-    <li class="flex lines " id="line${i}" >
+    <li class="flex lines " id="${i}" >
         <div class="flex line">
             <div class="flex">
                 ${taskWrite} 
-                <img src="./iconesordi/Vector.png" alt="doing" class="doing Naffiche">
+                <img src="./iconesordi/Vector.png" alt="doing" class="doing Naffiche" id="Doing">
                 <img src="./iconesordi/check.png" alt="Done" class="done Naffiche">
             </div>
 
             <div class="flex">
                 <!-- ${taskCreate.status} -->
-                <img src="./iconesordi/Vector-2.png"class="stylo pc line${i}" alt="modif" onclick="AffichFormEdit()">
-                <img src="./iconesTel/Vector-2.png"class="stylo tel line${i}" alt="modif" onclick="AffichFormEdit()">
+                <img src="./iconesordi/Vector-2.png"class="stylo pc ${i}" alt="modif" onclick="AffichFormEdit()">
+                <img src="./iconesTel/Vector-2.png"class="stylo tel ${i}" alt="modif" onclick="AffichFormEdit()">
 
-                <img src="./iconesordi/Vector-1.png" class="poubelle pc line${i}" alt="supr">
-                <img src="./iconesTel/Vector-1.png" class="poubelle tel line${i}" alt="supr">
+                <img src="./iconesordi/Vector-1.png" class="poubelle pc ${i}" alt="supr">
+                <img src="./iconesTel/Vector-1.png" class="poubelle tel ${i}" alt="supr">
 
             </div>
         </div> 
@@ -52,12 +57,63 @@ ulAdd.innerHTML += `
 
 const AffichFormEdit = () =>{
     classOfTask = event.target.className
-    console.log(classOfTask);
     formEdit.classList.add("affiche")
     formEdit.classList.remove("Naffiche")
 }
 
 
 const editTask = () =>{
-    
+    taskEditValue=document.getElementById("taskEditValue").value
+    taskEditStatues=document.getElementById("taskEditStatues").value
+    arrayClassOffTask=classOfTask.split(" ")
+    const number=arrayClassOffTask[2]
+    taskCreate[number] = {
+        value: taskEditValue,
+        status: taskEditStatues
+    }
+    Limodiff= document.getElementById(number)  
+    Limodiff.innerHTML= `
+        <div class="flex line">
+            <div class="flex">
+                ${taskCreate[number].value} 
+                <img src="./iconesordi/Vector.png" alt="doing" class="doing Naffiche"id="Doing${number}">
+                <img src="./iconesordi/check.png" alt="Done" class="done Naffiche" id="Done${number}">
+            </div>
+
+            <div class="flex">
+                <!-- ${taskCreate[number].status} -->
+                <img src="./iconesordi/Vector-2.png"class="stylo pc ${number}" alt="modif" onclick="AffichFormEdit()">
+                <img src="./iconesTel/Vector-2.png"class="stylo tel ${number}" alt="modif" onclick="AffichFormEdit()">
+
+                <img src="./iconesordi/Vector-1.png" class="poubelle pc ${number}" alt="supr">
+                <img src="./iconesTel/Vector-1.png" class="poubelle tel ${number}" alt="supr">
+
+            </div>
+        </div> 
+        <Button>Priorité</Button>  
+        `
+        Doing=document.getElementById(`Doing${number}`)
+        Done=document.getElementById(`Done${number}`)
+
+        console.log(Doing,Done);
+
+        if (taskCreate[number].status==="Doing"){
+            Doing.classList.remove("Naffiche")
+            Doing.classList.add("affiche")
+            Done.classList.remove("affiche")
+            Done.classList.add("Naffiche")
+        }else if (taskCreate[number].status==="Done"){
+            Doing.classList.remove("affiche")
+            Doing.classList.add("Naffiche")
+            Done.classList.remove("Naffiche")
+            Done.classList.add("affiche")
+        }else if (taskCreate[number].status==="To do"){
+            Doing.classList.remove("Naffiche")
+            Doing.classList.add("Naffiche")
+            Done.classList.remove("Naffiche")
+            Done.classList.add("Naffiche")
+        }
+
+    formEdit.classList.add("Naffiche")
+    formEdit.classList.remove("affiche")
 }
