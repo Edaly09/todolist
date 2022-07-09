705bf94fd9bce411d792073ba1dea7171afaf133
Thomas <malifarge.thomas9@gmail.com>
2022-07-09 12:28:41 +0200

    random + filter in index.js

--- index.js
+++ index.js
@@ -185,3 +185,27 @@ const randomTask = () =>{
 buttonRandom = document.getElementById("Random")
 
 buttonRandom.addEventListener("click", randomTask)
+
+// Filtre
+
+const statusFilter = () =>{
+    theFiltre = event.target.className
+    if (theFiltre==="All"){
+        filterArray = taskArray
+        console.log(filterArray);
+    }else{
+        taskArray.forEach(element=>{
+            console.log(element.status,theFiltre );
+            if (element.status===theFiltre){
+                filterArray.push(element)
+            }
+        })
+    }
+    console.log(filterArray);
+    ulAdd.innerHTML=``
+    i=0
+    filterArray.forEach(element=>{
+        printTask(element)
+    })
+    filterArray=[]
+}
